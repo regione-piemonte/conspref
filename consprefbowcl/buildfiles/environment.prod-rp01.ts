81d30a793f1a92b01d06094c50415f47aee1a3b3
CHIAPPO Maurizio 1469 <maurizio.chiappo@csi.it>
2022-06-15 16:05:53 +0200

    prima versione consprefbowcl

---
+++ consprefbowcl/buildfiles/environment.prod-rp01.ts
@@ -0,0 +1,16 @@
+/*******************************************************************************
+* Copyright Regione Piemonte - 2022
+* SPDX-License-Identifier: EUPL-1.2
+******************************************************************************/
+export const environment = {
+  production: true,
+  fakeTokenProd: false,
+  tokeName: 'Shib-Iride-IdentitaDigitale',
+  token: 'xyz/xyz/xyz/xyz/xyz/2/',
+  consprefboUrl: 'https://xyz.csi.it/consprefboweb/api',
+  auraAllowed: 'auraAllowed',
+  isDebugMode: true,
+  XCodiceServizi: 'consprefbo',
+  consprefboLogout: 'https://xyz.csi.it/xyz/Shibboleth.sso/Logout',
+  urlPua: 'https://xyz/puawa/sceltaRuoli',
+};
